204ae64b066ad02411dfc2b51872fb6e8fec4ed3
horacio cano <hhoraciocano@gmail.com>
2023-05-23 15:25:33 -0300

    basta de deploy

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
+require("dotenv").config()
 const server = require("./src/app.js");
 const { conn } = require("./src/db.js");
-const { PORT } = process.env || 3001
+const port = process.env.PORT || 3001
 // Syncing all the models at once.
 
 conn.sync({ force: false }).then(() => {
-  server.listen( PORT, () => {
+  server.listen( port, () => {
     console.log("%s listening at 3001"); // eslint-disable-line no-console
   });
 });
