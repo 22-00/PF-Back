68899e75048f8fd236ff0ec750d193a84639d549
horacio cano <hhoraciocano@gmail.com>
2023-05-23 15:44:44 -0300

    bueno

--- src/db.js
+++ src/db.js
@@ -3,10 +3,10 @@ const { Sequelize } = require('sequelize');
 const fs = require('fs');
 const path = require('path');
 const {
-  DB_USER, DB_PASSWORD, DB_HOST,DB_PORT, DB_NAME
+  DB_USER, DB_PASSWORD, DB_HOST,DB_PORT, DB_NAME, DB_DEPLOY
 } = process.env;
 
-const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`, {
+const sequelize = new Sequelize(DB_DEPLOY, {
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
 });
@@ -58,7 +58,7 @@ Address.belongsToMany(User, { through: 'UserAddress' });
 
 // comentario de prueba
 
-
+// `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`
 module.exports = {
   ...sequelize.models, // para poder importar los modelos así: const { Product, User } = require('./db.js');
   conn: sequelize,     // para importart la conexión { conn } = require('./db.js');
