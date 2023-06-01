29c4bd8ed54b35d97491c381b3b0535b5a92fd3d
horacio cano <hhoraciocano@gmail.com>
2023-06-01 04:18:56 -0300

    commit del forse en true

--- src/app.js
+++ src/app.js
@@ -14,9 +14,9 @@ server.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
 server.use(bodyParser.json({ limit: '50mb' }));
 server.use(cookieParser());
 server.use(morgan('dev'));
-server.use(cors()); ////
+server.use(cors()); 
 server.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin','http://localhost:3000', 'http://localhost:3002'); // update to match the domain you will make the request from
+  res.header('Access-Control-Allow-Origin','http://localhost:3000', 'http://localhost:3002',"pf-back-production-b670.up.railway.app"); // update to match the domain you will make the request from
   // res.header('Access-Control-Allow-Origin','http://localhost:3002');
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
@@ -33,6 +33,6 @@ server.use((err, req, res, next) => { // eslint-disable-line no-unused-vars
   console.error(err);
   res.status(status).send(message);
 });
-// https://pf-front-one.vercel.app
+// 
 module.exports = server;
 
